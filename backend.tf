dc1cb5709a23f8e41854e11a0350f8dd7a84453e
JoseManuel134975 <josemanuel3606@gmail.com>
2025-04-02 11:44:21 +0200

    workflow automatizado para desplegar (eleccion) el bucket y su backend + tabla de dynamo creada con terraform

--- backend.tf
+++ backend.tf
@@ -1,21 +1,8 @@
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket         = "ia-automatizacion-jose-manuel-bucket"
-    key            = "./terraform/terraform.tfstate"  # Ruta del estado dentro del bucket
-    region         = "us-east-1"                  # Región del bucket
-    use_lockfile = true
-    encrypt        = true                         # Cifrado de datos en S3
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_state"
   }
 }
-
